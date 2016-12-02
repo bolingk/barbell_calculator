e43c976178a0297b8a9cabe06eeea526f5b3d9f9
Kyle Boling <kmboling@gmail.com>
2016-12-01 23:54:03 -0500

    first commit

---
+++ barbell_calculator.rb
@@ -0,0 +1,45 @@
+
+def calc
+  bar_weight = 45   # all possible weight configurations end in 5 for bar weight of 45lbs
+  plates = [45, 35, 25, 15, 10, 5]
+
+  weight = gets.chomp.to_i
+
+  while weight % 5 != 0
+    puts "Enter a weight in increments of 5 lbs:"
+    weight = gets.chomp.to_i
+  end 
+
+ 
+  if weight % 10 == 0
+    bar_less = weight - bar_weight + 5
+    puts "That weight is not possible with our current setup"
+    puts "Let me bump it up 5 lbs to #{weight + 5} lbs"
+    puts "For a weight of #{weight + 5} lbs you will need (on each side):"
+  else
+    bar_less = weight - bar_weight
+    puts "For a weight of #{weight} lbs you will need (on each side):"
+  end
+
+
+  plates.each do |plate|
+  	pair = plate * 2
+    if bar_less >= 10
+      num_pl = bar_less / pair
+      bar_less = bar_less % pair
+      if num_pl == 1
+        puts "#{num_pl} plate of #{plate}" 
+      elsif num_pl > 1
+      	puts "#{num_pl} plates of #{plate}" 
+      end
+    end
+  end
+end
+
+puts
+puts "Enter the desired weight (in lbs.) to load the bar:"
+
+calc
+
+
+
